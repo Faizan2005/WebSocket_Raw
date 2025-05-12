c15c3723989ff1855bd0067f7d613d386132d7a1
Faizan2005 <illusionbuster05@gmail.com>
2025-05-12 20:06:31 +0530

    feat: add Server handshake response

--- client.mjs
+++ client.mjs
@@ -1,12 +1,35 @@
-const messages = document.getElementById("messages")
-const socket = new WebSocket("ws://localhost:8000")
+const messages = document.getElementById("messages");
+const socket = new WebSocket("ws://localhost:8000");
 
 socket.onopen = (event) => {
-    
-}
+  console.log("WebSocket is connected...");
+  const data = JSON.stringify({
+    message: "Hello, firstName()! Your order number is: #int(1,100)",
+    phoneNumber: "phoneNumber()",
+    phoneVariation: "+90 int(300,399) int(100,999) int(10-99) int(10,99)",
+    status: "enum(active, disabled)",
+    name: {
+      first: "firstName()",
+      middle: "middleName()",
+      last: "lastName()",
+    },
+  });
 
+  const msgInterval = setInterval(() => {
+    socket.send(data), 2000
+  })
+};
 
+socket.onmessage = (msg) => {
+  const message = msg.data
+  console.log('I got a message!', message)
+  messages.innerHTML += `<br/> ${message}`
+};
 
-socket.onmessage = (msg) => {}
-socket.onclose = (event) => {}
-socket.onerror = (error) => {}
+socket.onclose = (event) => {
+  console.log('WebSocket server disconnected')
+};
+
+socket.onerror = (error) => {
+  console.error('WebSocket error', error)
+};
--- server.mjs
+++ server.mjs
@@ -1,5 +1,7 @@
 import {createServer} from 'http'
+import crypto from 'crypto'
 const PORT = 8000
+const MAGIC_STRING = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
 
 const server = createServer((req, res) => {
     res.writeHead(200)
@@ -10,4 +12,21 @@ server.listen(PORT, () => {
     console.log(`server listening to port ${PORT}`)
     console.log('http://localhost:8000')
 })
+
+server.on('upgrade', onSocketUpgrade)
+
+function onSocketUpgrade(req, socket, head) {
+    const {
+        'Sec-WebSocket-Accept': webClientSocketKey
+    } = req.headers
+
+
+}
+
+
+
+
+
+
+
 
